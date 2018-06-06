ce2920c6f934d9c789c48a4881737ced67a1aac9
BK95 <owen5836@naver.com>
2018-06-06 15:01:51 +0900

    Update TimeTableServiceImpl.java

--- spam00/src/com/spam/service/TimeTableServiceImpl.java
+++ spam00/src/com/spam/service/TimeTableServiceImpl.java
@@ -1,5 +1,43 @@
 package com.spam.service;
 
-public class TimeTableServiceImpl {
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spam.domain.TimeTable;
+import com.spam.persistence.TimeTableMapper;
+
+@Service
+public class TimeTableServiceImpl implements TimeTableService{
+
+		@Resource
+		private TimeTableMapper timeTableMapper;
+
+		@Override
+		public List<TimeTable> list(TimeTable timeTable) {
+			return this.timeTableMapper.list(timeTable);
+		}
+
+		@Override
+		@Transactional
+		public void add(TimeTable timeTable) {
+			
+			this.timeTableMapper.insert(timeTable);
+		}
+
+		@Override
+		public TimeTable view(TimeTable timeTable) {
+			return this.timeTableMapper.select(timeTable);
+		}
+
+		@Override
+		@Transactional
+		public void edit(TimeTable timeTable) {
+			this.timeTableMapper.update(timeTable);
+		}
+
 
 }
