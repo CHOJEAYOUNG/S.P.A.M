30795ce0f08b1778292cfb0cbed155faf7ca008d
BK95 <owen5836@naver.com>
2018-06-06 15:00:29 +0900

    Update AdviceServiceImpl.java

--- spam00/src/com/spam/service/AdviceServiceImpl.java
+++ spam00/src/com/spam/service/AdviceServiceImpl.java
@@ -1,5 +1,62 @@
 package com.spam.service;
 
-public class AdviceServiceImpl {
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spam.domain.Advice;
+import com.spam.persistence.AdviceMapper;
+
+
+@Service
+public class AdviceServiceImpl implements AdviceService{
+
+		@Resource
+		private AdviceMapper adviceMapper;
+
+		@Override
+		public List<Advice> list(Advice advice) {
+			return this.adviceMapper.list(advice);
+		}
+
+		@Override
+		@Transactional
+		public void add(Advice advice) {
+			long time = System.currentTimeMillis();
+		      SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-MM-dd");
+		      String str = dayTime.format(new Date(time));
+		      advice.setRegistration(str);
+			this.adviceMapper.insert(advice);
+	}
+
+		@Override
+		public Advice view(Advice advice) {
+		return this.adviceMapper.select(advice);
+		}
+
+		@Override
+		@Transactional
+		public void edit(Advice advice) {
+			long time = System.currentTimeMillis();
+		      SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-MM-dd");
+		      String str = dayTime.format(new Date(time));
+		      advice.setAssentDate(str);
+		     
+		      System.out.println(advice.toString());
+			this.adviceMapper.update(advice);
+		}
+		
+		@Override
+		@Transactional
+		public void delete(Advice advice) {
+			this.adviceMapper.delete(advice);
+		}
+
 
 }
