ccce197f6e721ba7f9eb7d10cfb401decc0ff2e0
BK95 <owen5836@naver.com>
2018-06-06 14:57:38 +0900

    Update TimeTable.java

--- spam00/src/com/spam/domain/TimeTable.java
+++ spam00/src/com/spam/domain/TimeTable.java
@@ -1,5 +1,33 @@
 package com.spam.domain;
 
-public class TimeTable {
+import java.util.Date;
 
+public class TimeTable{
+	private int period;
+	private String begin;
+	private String finish;
+	
+	@Override
+	public String toString() {
+		return "TimeTable [period=" + period + ", begin=" + begin + ", finish=" + finish + "]";
+	}
+	
+	public int getPeriod() {
+		return period;
+	}
+	public void setPeriod(int period) {
+		this.period = period;
+	}
+	public String getBegin() {
+		return begin;
+	}
+	public void setBegin(String begin) {
+		this.begin = begin;
+	}
+	public String getFinish() {
+		return finish;
+	}
+	public void setFinish(String finish) {
+		this.finish = finish;
+	}
 }
