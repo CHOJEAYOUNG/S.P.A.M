d0056a0d72a30df83cc81c8dd51f2f07bd28aaee
BK95 <owen5836@naver.com>
2018-06-06 15:01:15 +0900

    Update ScheduleServiceImpl.java

--- spam00/src/com/spam/service/ScheduleServiceImpl.java
+++ spam00/src/com/spam/service/ScheduleServiceImpl.java
@@ -1,5 +1,49 @@
 package com.spam.service;
 
-public class ScheduleServiceImpl {
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spam.domain.Schedule;
+import com.spam.persistence.ScheduleMapper;
+
+@Service
+public class ScheduleServiceImpl implements ScheduleService{
+
+		@Resource
+		private ScheduleMapper scheduleMapper;
+
+		@Override
+		public List<Schedule> list(Schedule schedule) {
+			return this.scheduleMapper.list(schedule);
+		}
+
+		@Override
+		@Transactional
+		public void add(Schedule schedule) {
+			schedule.setQuarter(0);
+			this.scheduleMapper.insert(schedule);
+		}
+
+		@Override
+		public Schedule view(Schedule schedule) {
+			return this.scheduleMapper.select(schedule);
+		}
+
+		@Override
+		@Transactional
+		public void edit(Schedule schedule) {
+			this.scheduleMapper.update(schedule);
+		}
+		
+		@Override
+		@Transactional
+		public void delete(Schedule schedule) {
+			this.scheduleMapper.delete(schedule);
+		}
+
 
 }
