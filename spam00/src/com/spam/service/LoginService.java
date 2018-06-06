6c18029410fdb15ea0183525e090b01a9abd8cdc
BK95 <owen5836@naver.com>
2018-06-06 15:04:21 +0900

    Update LoginService.java

--- spam00/src/com/spam/service/LoginService.java
+++ spam00/src/com/spam/service/LoginService.java
@@ -1,12 +1,1 @@
-package com.spam.service;
-
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
-import com.spam.domain.SpamUser;
-
-public interface LoginService {
-	public boolean login(SpamUser spamuser, HttpServletRequest request);
-	public List<SpamUser> list(HttpServletRequest request, SpamUser spamuser);
-}
+public List<SpamUser> listPop(SpamUser spamuser);
