4f289e181f560671dae5d64df573217635e2b3b1
BK95 <owen5836@naver.com>
2018-06-06 15:00:51 +0900

    Update ScheduleService.java

--- spam00/src/com/spam/service/ScheduleService.java
+++ spam00/src/com/spam/service/ScheduleService.java
@@ -1,5 +1,19 @@
 package com.spam.service;
 
-public class ScheduleService {
 
+import java.util.List;
+
+import com.spam.domain.Schedule;
+
+
+public interface ScheduleService {
+	public List<Schedule> list(Schedule schedule);
+
+	public void add(Schedule schedule);
+
+	public Schedule view(Schedule schedule);
+
+	public void edit(Schedule schedule);
+	
+	public void delete(Schedule schedule);
 }
