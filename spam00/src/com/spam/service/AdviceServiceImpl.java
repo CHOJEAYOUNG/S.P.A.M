f8f4127c91509e87c1ee8ef6b965d47b9a38b1cf
BK95 <owen5836@naver.com>
2018-06-11 14:57:49 +0900

    Update AdviceServiceImpl.java

--- spam00/src/com/spam/service/AdviceServiceImpl.java
+++ spam00/src/com/spam/service/AdviceServiceImpl.java
@@ -24,7 +24,17 @@ public class AdviceServiceImpl implements AdviceService{
 		public List<Advice> list(Advice advice) {
 			return this.adviceMapper.list(advice);
 		}
-
+		
+		@Override
+		public List<Advice> listS(Advice advice) {
+			return this.adviceMapper.listS(advice);
+		}
+		
+		@Override
+		public List<Advice> listF(Advice advice) {
+			return this.adviceMapper.listF(advice);
+		}
+		
 		@Override
 		@Transactional
 		public void add(Advice advice) {
@@ -32,6 +42,7 @@ public class AdviceServiceImpl implements AdviceService{
 		      SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-MM-dd");
 		      String str = dayTime.format(new Date(time));
 		      advice.setRegistration(str);
+		      advice.setAssentNo(2);
 			this.adviceMapper.insert(advice);
 	}
 
